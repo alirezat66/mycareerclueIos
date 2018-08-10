c882e033cdede0f383943e91ba90fe974dc42e4d
alireza <alirezataghizadeh66@gmail.com>
2018-08-10 16:58:21 +0430

    ali

--- WeYouMaster/Controller/LogInVC.swift
+++ WeYouMaster/Controller/LogInVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LogInVC: UIViewController {
 
+    @IBOutlet weak var edtEmail: UITextField!
+    @IBOutlet weak var edtPass: UITextField!
     @IBAction func backBtn(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
@@ -19,6 +21,54 @@ class LogInVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnLogin(_ sender: Any) {
+      
+        if((edtEmail.text?.count)!>0){
+            if((edtPass.text?.count)!>0){
+                  let email = edtEmail.text
+                  let pass = edtPass.text
+                
+                let myUrl = NSURL(string: "https://www.weyoumaster.com/api/login/")
+                var request  = URLRequest(url: myUrl! as URL)
+                request.httpMethod = "POST"
+                let postString = "emailaddress=" + email! + "&password=" + pass!
+                request.httpBody = postString.data(using: String.Encoding.utf8)
+               let session = URLSession.shared
+               
+                let task = session.dataTask(with: request){
+                    (data: Data?, response: URLResponse?, error: Error?) in
+                    
+                    if error != nil
+                    {
+                        print("error=\(error)")
+                        return
+                    }
+                    
+                    // You can print out response object
+                    print("response = \(response)")
+                    
+                    //Let's convert response sent from a server side script to a NSDictionary object:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                        print(json!)
+                     /*   if let parseJSON = json {
+                            
+                            // Now we can access value of First Name by its key
+                            let firstNameValue = parseJSON["firstName"] as? String
+                            print("firstNameValue: \(firstNameValue)")
+                        }*/
+                    } catch {
+                        print(error)
+                    }
+                }
+                task.resume()
+                
+                
+                    
+                
+            }
+        }
+    }
+    
 
 }
